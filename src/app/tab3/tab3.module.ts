af23788841dca204feaea720f5f01fc3ee66d297
Ramayana A. Menezes Junior <ramayana.menezes@tjam.jus.br>
2019-10-03 23:41:18 -0400

    Adição de Funcionalidades * Alert + Input * Service Local Storage * Exibição lista de toggles

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import { TopicosService } from '../services/topicos.service';
 
 @NgModule({
   imports: [
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { TopicosService } from '../services/topicos.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,43 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(
+    private alertController: AlertController,
+    private topService: TopicosService) {}
 
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Adicionar Tópico!',
+      inputs: [
+        {
+          name: 'nomeTopico',
+          id: 'nome',
+          type: 'text',
+          placeholder: 'Nome do tópico'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+            this.topService.addTopico(data.nomeTopico);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  setEstado(event) {
+    this.topService.setTopicos();
+  }
 }
